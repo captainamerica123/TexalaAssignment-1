8a0d5ef8ddb5637bf08337cd74ae2c386f9a6553
Dell <Dell@192.168.18.180>
2022-02-19 16:43:08 +0530

    First Commit

---
+++ src/com/bhushan/main/texalaassig1.java
@@ -0,0 +1,16 @@
+package com.bhushan.main;
+
+import java.io.IOException;
+
+import com.bhushan.logic.Service;
+
+public class texalaassig1 extends Service {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Service s1=new Service();
+		s1.ReadHTML();
+		s1.WriteHTMLToFile();
+	}
+
+}
---
+++ src/com/bhushan/testcase/Junit.java
@@ -0,0 +1,19 @@
+package com.bhushan.testcase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Junit {
+
+	@Test
+	void testReadHTML() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWriteHTMLToFile() {
+		fail("Not yet implemented");
+	}
+
+}
